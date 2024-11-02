69cf077ce710287d953d34dc7f900378df60e55a
Hoang An <annguyen.pers@gmail.com>
2024-11-02 23:27:00 +0100

    update

--- utils/functions.py
+++ utils/functions.py
@@ -2,18 +2,17 @@ import pandas as pd
 import numpy as np
 from dateutil import parser
 import isodate
+
 from itertools import chain
 
-import sys
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 
-import sqlalchemy
-from sqlalchemy import create_engine
-import mysql.connector
-import pymysql
+import os
+from dotenv import load_dotenv
 
-# Get channel info first
+load_dotenv() 
+
+# Get channel info
 def get_channel_stats(youtube, handles):
     all_data = []
     for i in handles:
@@ -50,7 +49,7 @@ def get_video_ids(youtube, playlist_id):
     
     #Get more ids if there is more pages
     next_page_token = response_vi.get('nextPageToken')
-    
+
     more_pages = True
     while more_pages:
         if next_page_token is None:
@@ -64,6 +63,7 @@ def get_video_ids(youtube, playlist_id):
             response_vi = request_vi.execute()
             video_ids = list(chain(video_ids,[item['contentDetails']['videoId'] for item in response_vi['items']]))
             next_page_token = response_vi.get('nextPageToken')
+
     return  video_ids
 
 # Extract information for each video id.
@@ -85,7 +85,7 @@ def get_video_details(youtube, video_ids):
                 'contentDetails': ['duration', 'definition', 'caption']
             }
             
-            video_info = {}  # Dictionary for storing information for one video
+            video_info = {}
             
             # Iterate over the top-level keys (e.g., snippet, statistics)
             for k, v_list in stats_to_keep.items():
@@ -95,7 +95,7 @@ def get_video_details(youtube, video_ids):
                         if k == 'id':
                             video_info[v] = video.get(v)
                         else:
-                            video_info[v] = video[k].get(v, None)  # Use .get to avoid KeyError
+                            video_info[v] = video[k].get(v, None)
                     except KeyError:
                         video_info[v] = None
             
@@ -103,22 +103,22 @@ def get_video_details(youtube, video_ids):
                 video_info['tags'] = ', '.join(video_info['tags'])
             else:
                 video_info['tags'] = None  # Handle case where tags are missing or not a list
-            
-            videos_info.append(video_info)  # Append this video's information to the list
-        
-        # video_info now contains the last processed video
-    videos_details = pd.DataFrame(videos_info) # This contains all processed video data
+            videos_info.append(video_info)
+    videos_details = pd.DataFrame(videos_info)
+
     return videos_details
 
 def connect_youtube( api_key):
     youtube = googleapiclient.discovery.build(
         'youtube', 'v3', developerKey=api_key)
+    
     return youtube
 
 def data_channel_youtube(youtube,handles):
     channel_data = get_channel_stats(youtube, handles)
     numeric_cols = ['subscribers', 'views', 'totalVideos']
     channel_data[numeric_cols] = channel_data[numeric_cols].apply(pd.to_numeric, errors='coerce')
+
     return channel_data
 
 def data_video_youtube(youtube,channel_data):
@@ -129,6 +129,7 @@ def data_video_youtube(youtube,channel_data):
         video_ids = get_video_ids(youtube, playlist_id)
         video_data = get_video_details(youtube, video_ids)
         video_df = pd.concat([video_df, video_data], ignore_index=True)
+
     return video_df
 
 def transform_data(video_df):
@@ -138,8 +139,21 @@ def transform_data(video_df):
     video_df2['publishedAt'] = video_df['publishedAt'].apply(lambda x: parser.parse(x))
     video_df2['publishedOn'] = video_df2['publishedAt'].apply(lambda x: x.strftime("%A"))
     video_df2['duration'] = video_df2['duration'].apply(lambda x: isodate.parse_duration(x))
+    video_df2['duration'] = video_df2['duration'].dt.total_seconds()
     video_df2['datepublished'] = video_df2['publishedAt'].dt.date
     video_df2['timepublished'] = video_df2['publishedAt'].dt.time
     video_df2.drop(columns = ['publishedAt'], inplace = True)
-    video_df2['duration'] = video_df2['duration'].dt.total_seconds()  # Converts to seconds as float
+
     return video_df2
+
+# Ready to use function
+def get_data_youtue(handles):
+    youtube = connect_youtube(os.getenv('API_KEY'))
+    channel_data = data_channel_youtube(youtube,handles)
+    video_df = data_video_youtube(youtube,channel_data)
+    df = transform_data(video_df)
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    df.to_csv('data/youtube_data.csv', index=False)
+    print('Done!')
